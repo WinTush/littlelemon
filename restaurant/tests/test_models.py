c5b324542c8170e4201c4137c6c08464f90ee47d
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-12 13:22:56 +0300

    test(restaurant): provide values for tests

--- restaurant/tests/test_models.py
+++ restaurant/tests/test_models.py
@@ -1,17 +1,17 @@
 import pytest
-
-from restaurant.models import Booking, Menu
+from django.utils import timezone
 
 pytestmark = pytest.mark.django_db
 
 
 class TestMenuModel:
     def test_str_method(self, menu_factory):
-        menu: Menu = menu_factory()
-        assert str(menu) == menu.title
+        menu = menu_factory(title="test_menu")
+        assert str(menu) == "test_menu"
 
 
 class TestBookingModel:
     def test_str_method(self, booking_factory):
-        booking: Booking = booking_factory()
-        assert str(booking) == f"{booking.name}: {booking.booking_date}"
+        time_now = timezone.now()
+        booking = booking_factory(name="John Doe", booking_date=time_now)
+        assert str(booking) == f"John Doe: {time_now}"
