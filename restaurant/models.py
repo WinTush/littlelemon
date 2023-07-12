3dad3a2a20ef1a6bc7296ea23df32b1a1ca34e03
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-12 12:34:36 +0300

    feat(restaurant): add Menu and Booking models
      - Implemented Menu and Booking models in models.py
      - Created initial migration file for the models
      - Registered Menu and Booking models in the admin site

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,1 +1,19 @@
-# from django.db import models
+from django.db import models
+
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.SmallIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField(default=0)
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name}: {self.booking_date}"
