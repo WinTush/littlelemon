280e5b46adb5c4e5ec17d411ed4d4aee9b7d4f7c
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-15 19:52:11 +0300

    feat(api): add API endpoints for menu
      - Use views from generics.rest_framework for CRUD operations
      - Implement views for menu list and menu detail
      - Add endpoints for menu list and menu detail views in `api.urls`

--- LittleLemon/urls.py
+++ LittleLemon/urls.py
@@ -21,6 +21,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurant/", include("restaurant.urls")),
+    path("api/", include("api.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
 ]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,15 @@
-# from django.shortcuts import render
+from rest_framework import generics
+
+from restaurant.models import Menu
+
+from .serializers import MenuSerializer
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
