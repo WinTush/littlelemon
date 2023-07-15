d7186cc557b05f8e94a73c0d1c8f6c7d92815ec1
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-15 19:56:51 +0300

    feat(api): add menu item API tests
      - Add tests for menu item GET, POST, PATCH, and DELETE operations
      - Test the status code, response data, and expected behavior

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
+import pytest
 from pytest_factoryboy import register
+from rest_framework.test import APIClient
 
 from .test_restaurant.factories import BookingFactory, MenuFactory
 
 register(BookingFactory)
 register(MenuFactory)
+
+
+@pytest.fixture
+def api_client():
+    return APIClient
