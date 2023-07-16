4c683df8a94140eeab2bdf398f29f30edf645432
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-16 06:20:30 +0300

    test(api): add booking view tests
      - Add tests for the booking views in test_views.py
      - Test GET, POST, PUT, and DELETE operations
      - Test status codes, data consistency, and response content

--- tests/test_api/test_views.py
+++ tests/test_api/test_views.py
@@ -53,3 +53,54 @@ class TestSingleMenuItemView:
         response = api_client().delete(url)
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
+
+
+class TestBookingViewSet:
+    def test_status_code(self, api_client):
+        url = reverse("booking-list")
+        response = api_client().get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+
+    def test_create_booking(self, api_client):
+        url = reverse("booking-list")
+        data = {
+            "name": "John Doe",
+            "no_of_guests": 4,
+            "booking_date": "2022-01-01T12:00:00Z",
+        }
+        response = api_client().post(url, data)
+
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.data["name"] == data["name"]
+        assert response.data["no_of_guests"] == data["no_of_guests"]
+        assert response.data["booking_date"] == data["booking_date"]
+
+    def test_get_booking(self, api_client, booking_factory):
+        booking = booking_factory(name="John Doe")
+        url = reverse("booking-detail", args=[booking.id])
+        response = api_client().get(url)
+
+        assert response.data.get("name") == booking.name
+
+    def test_update_booking(self, api_client, booking_factory):
+        booking = booking_factory(name="John Doe", no_of_guests=4)
+        url = reverse("booking-detail", args=[booking.id])
+        data = {
+            "name": "Jane Doe",
+            "no_of_guests": 6,
+            "booking_date": "2022-01-02T12:00:00Z",
+        }
+        response = api_client().put(url, data)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data["name"] == data["name"]
+        assert response.data["no_of_guests"] == data["no_of_guests"]
+        assert response.data["booking_date"] == data["booking_date"]
+
+    def test_delete_booking(self, api_client, booking_factory):
+        booking = booking_factory()
+        url = reverse("booking-detail", args=[booking.id])
+        response = api_client().delete(url)
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
