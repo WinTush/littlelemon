00ae41e5b72e75f9b024e5dc94ae462e05ff2156
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-16 15:13:37 +0300

    feat(api): add permission classes to API views
      - Add permission classes to MenuViewSet and BookingViewSet
      - Use IsAuthenticatedOrReadOnly for BookingViewSet
      - Use IsAdminUser for MenuViewSet

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
 from restaurant.models import Booking, Menu
 
@@ -9,7 +10,13 @@ class MenuViewSet(viewsets.ModelViewSet):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return []
+        return [IsAdminUser()]
+
 
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
