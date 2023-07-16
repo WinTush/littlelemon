ded0238f10d19babd3ac5621cf14acdb9b4aadcf
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-16 15:28:58 +0300

    test(api): add authentication and authorization tests
      - Create fixtures for authenticated and admin clients for testing different
      user roles
      - Add authentication and authorization tests for creating, updating, and deleting
      menu items and bookings

--- tests/test_api/test_views.py
+++ tests/test_api/test_views.py
@@ -13,9 +13,21 @@ class TestMenuListView:
 
         assert response.status_code == status.HTTP_200_OK
 
-    def test_create_menu_item(self, api_client):
+    def test_unauthenticated_create_menu_item(self, api_client):
         data = {"title": "Pizza", "price": "10.99"}
         response = api_client.post(self.url, data)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_unauthorized_create_menu_item(self, authenticated_client):
+        data = {"title": "Pizza", "price": "10.99"}
+        response = authenticated_client.post(self.url, data)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_admin_create_menu_item(self, admin_client):
+        data = {"title": "Pizza", "price": "10.99"}
+        response = admin_client.post(self.url, data)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data["title"] == data["title"]
@@ -37,20 +49,35 @@ class TestMenuDetailView:
 
         assert response.data.get("title") == menu.title
 
-    def test_update_menu_item(self, api_client, menu_factory):
+    def test_unauthorized_update_menu_item(self, authenticated_client, menu_factory):
         menu = menu_factory(title="Burger", price=9.99)
         url = reverse("menu-detail", args=[menu.id])
         data = {"title": "Cheeseburger", "price": "12.99"}
-        response = api_client.patch(url, data)
+        response = authenticated_client.patch(url, data)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_update_menu_item(self, admin_client, menu_factory):
+        menu = menu_factory(title="Burger", price=9.99)
+        url = reverse("menu-detail", args=[menu.id])
+        data = {"title": "Cheeseburger", "price": "12.99"}
+        response = admin_client.patch(url, data)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data["title"] == data["title"]
         assert response.data["price"] == data["price"]
 
-    def test_delete_menu_item(self, api_client, menu_factory):
+    def test_unauthorized_delete_menu_item(self, authenticated_client, menu_factory):
         menu = menu_factory()
         url = reverse("menu-detail", args=[menu.id])
-        response = api_client.delete(url)
+        response = authenticated_client.delete(url)
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
+
+    def test_delete_menu_item(self, admin_client, menu_factory):
+        menu = menu_factory()
+        url = reverse("menu-detail", args=[menu.id])
+        response = admin_client.delete(url)
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
 
@@ -63,13 +90,23 @@ class TestBookingListView:
 
         assert response.status_code == status.HTTP_200_OK
 
-    def test_create_booking(self, api_client):
+    def test_unauthenticated_create_booking(self, api_client):
         data = {
             "name": "John Doe",
             "no_of_guests": 4,
             "booking_date": "2022-01-01T12:00:00Z",
         }
         response = api_client.post(self.url, data)
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_authenticated_create_booking(self, authenticated_client):
+        data = {
+            "name": "John Doe",
+            "no_of_guests": 4,
+            "booking_date": "2022-01-01T12:00:00Z",
+        }
+        response = authenticated_client.post(self.url, data)
 
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data["name"] == data["name"]
@@ -85,7 +122,7 @@ class TestBookingDetailView:
 
         assert response.data.get("name") == booking.name
 
-    def test_update_booking(self, api_client, booking_factory):
+    def test_update_booking(self, authenticated_client, booking_factory):
         booking = booking_factory(name="John Doe", no_of_guests=4)
         url = reverse("booking-detail", args=[booking.id])
         data = {
@@ -93,16 +130,16 @@ class TestBookingDetailView:
             "no_of_guests": 6,
             "booking_date": "2022-01-02T12:00:00Z",
         }
-        response = api_client.put(url, data)
+        response = authenticated_client.put(url, data)
 
         assert response.status_code == status.HTTP_200_OK
         assert response.data["name"] == data["name"]
         assert response.data["no_of_guests"] == data["no_of_guests"]
         assert response.data["booking_date"] == data["booking_date"]
 
-    def test_delete_booking(self, api_client, booking_factory):
+    def test_delete_booking(self, authenticated_client, booking_factory):
         booking = booking_factory()
         url = reverse("booking-detail", args=[booking.id])
-        response = api_client.delete(url)
+        response = authenticated_client.delete(url)
 
         assert response.status_code == status.HTTP_204_NO_CONTENT
