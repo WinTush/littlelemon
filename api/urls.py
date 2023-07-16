cbd52b320681d9f7ce80b8b33b5cdcfc6cf2632b
Winston Kinoti <prog.kinoti@gmail.com>
2023-07-16 06:17:13 +0300

    feat(api): add booking viewset and endpoint
      - Created a viewset for handling CRUD operations on bookings
      - Registered the booking viewset in the API's URL configuration
      - Added a new endpoint for booking resources

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
 from . import views
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"booking", views.BookingViewSet)
 
 urlpatterns = [
     path("menu", views.MenuItemsView.as_view(), name="menu-list"),
     path("menu/<int:pk>", views.SingleMenuItemView.as_view(), name="menu-detail"),
+    path("", include(router.urls)),
 ]
